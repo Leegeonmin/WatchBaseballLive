88788fe1c165fe8a91b849b9a600e008cc1b7a88
Leegeonmin <kmlee97s@gmail.com>
2024-07-04 18:11:21 +0900

    initial commit

---
+++ src/main/java/com/bongbong/watchbaseball/WatchbaseballApplication.java
@@ -0,0 +1,13 @@
+package com.bongbong.watchbaseball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchbaseballApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatchbaseballApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bongbong/watchbaseball/WatchbaseballApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bongbong.watchbaseball;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchbaseballApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
