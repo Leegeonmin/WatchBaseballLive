2e42c3336ff36e57fb1a5f02b0830fe2c86b34e1
Leegeonmin <kmlee97s@gmail.com>
2025-03-12 06:36:49 +0900

    feat : 지역 코드 상수 클래스 생성

---
+++ src/main/java/com/bongbong/watchbaseball/constant/LocationCode.java
@@ -0,0 +1,11 @@
+package com.bongbong.watchbaseball.constant;
+
+public final class LocationCode {
+    private LocationCode() {}
+    public static final String GYEONGSANGDO = "11H20000";
+    public static final String DAEGU = "11H10000";
+    public static final String SEOUL = "11B00000";
+    public static final String GWANGJU = "11F20000";
+    public static final String DAEJEON = "11C20000";
+
+}
