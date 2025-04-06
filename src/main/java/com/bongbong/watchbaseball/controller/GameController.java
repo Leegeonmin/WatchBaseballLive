2a8f74b0af081085b3e1782fcc37679cd88e970b
Leegeonmin <kmlee97s@gmail.com>
2025-04-07 01:54:42 +0900

    feat : weather 갱신 로직, 스케쥴러 추가

--- src/main/java/com/bongbong/watchbaseball/controller/GameController.java
+++ src/main/java/com/bongbong/watchbaseball/controller/GameController.java
@@ -10,7 +10,6 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +18,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/game")
@@ -26,33 +27,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class GameController {
 
   private final GameService gameService;
-
-
-//  /**
-//   * 경기일정 검색 API
-//   *
-//   * @param teamname 팀이름
-//   * @return 상대팀이름과 경기날짜로 구성된 리스트
-//   */
-//  @Operation(summary = "팀의 경기 일정을 조회")
-//  @GetMapping
-//  @ApiResponses(value = {
-//      @ApiResponse(responseCode = "200", description = "Success",
-//          content = {
-//              @Content(array = @ArraySchema(schema = @Schema(implementation = GetGameListByTeamNameResponse.class)))}),
-//      @ApiResponse(responseCode = "404", description = "Not Found",
-//          content = {
-//              @Content(array = @ArraySchema(schema = @Schema(implementation = CustomErrorResponse.class)))}),
-//  })
-//  public ResponseEntity<List<GetGameListByTeamNameResponse>> getGamesByName(
-//      @RequestParam(name = "teamname")
-//      @Schema(description = "허용된 값: LOTTE, DOSAN," +
-//          " KIA, SAMSUNG, SSG, NC, LG, KIWOOM, " +
-//          "KT, HANWHA", example = "LG")
-//      String teamname) {
-//    return ResponseEntity.ok(gameService.findGamesByTeamName(teamname));
-//  }
-
   /**
    * 경기일정 검색 API
    *
