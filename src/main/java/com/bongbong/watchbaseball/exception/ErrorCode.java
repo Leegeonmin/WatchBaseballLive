fa52cc4b8e23602d1ec190acaea9a080b2617be8
Leegeonmin <kmlee97s@gmail.com>
2025-04-07 01:23:06 +0900

    feat : errorCode 추가 (유효한 날짜를 찾을 수 없는 경우)

--- src/main/java/com/bongbong/watchbaseball/exception/ErrorCode.java
+++ src/main/java/com/bongbong/watchbaseball/exception/ErrorCode.java
@@ -7,9 +7,10 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
     INTERNAL_SERVER_ERROR("서버 에러입니다 문의주세요", HttpStatus.INTERNAL_SERVER_ERROR),
     BAD_REQUEST("유효하지않은 요청입니다", HttpStatus.BAD_REQUEST),
-    TEAM_NOT_FOUND("팀을 찾지 못했습니다",HttpStatus.NOT_FOUND);
+    TEAM_NOT_FOUND("팀을 찾지 못했습니다",HttpStatus.NOT_FOUND),
+    WEATHER_DATE_ERROR("유효한 날짜를 찾을 수 없습니다", HttpStatus.INTERNAL_SERVER_ERROR)
 
-
+    ;
     private final String description;
     private final HttpStatus httpStatus;
 
